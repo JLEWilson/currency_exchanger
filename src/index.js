433986181137a5c2d3cb6ebec18c4f565d0b6264
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-19 08:12:41 -0900

    Add populateDropdown, change callForUsd to callForCurrencies

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import './css/styles.css';
 import $ from 'jquery';
 import CurrencyExchange from './js/currencyExchange-service';
 
-async function callForUSDRates(){
+function populateDropdown(id, reference){
+  let currencies = Object.keys(reference.conversion_rates);
+  for(let i = 0; i < currencies.length; i++){
+    $(`#${id}`).append("<option'>" + currencies[i] + "</option>")
+  }
+}
+async function callForCurrencies(id){
   const response = await CurrencyExchange.getUSDExchangeRates();
+  populateDropdown(id, response);
+}
+async function callForAnyRate(convertFrom){
+  const response = await CurrencyExchange.getAnyExchangeRate(convertFrom);
+  console.log(response);
   return response;
 }
-
-callForUSDRates();
+callForAnyRate("USD");
