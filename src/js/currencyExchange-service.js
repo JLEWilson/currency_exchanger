f9ecdfbd0211966d8598d925bec03668cd1efbda
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-19 08:14:17 -0900

    remove indentation errors

--- src/js/currencyExchange-service.js
+++ src/js/currencyExchange-service.js
@@ -1,8 +1,18 @@
-
 export default class CurrencyExchange{
   static async getUSDExchangeRates(){
     try{
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      if(!response.ok){
+        throw Error(response.statusText);
+      }
+      return await response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
+  static async getAnyExchangeRate(convertFrom){
+    try{
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${convertFrom}`);
       if(!response.ok){
         throw Error(response.statusText);
       }
