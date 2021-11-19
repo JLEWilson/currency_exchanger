a317218b10ff3c5586bdc4fe835c7673f7d1c38a
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-11-19 07:38:21 -0900

    add callForUSDRates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import $ from 'jquery';
+import CurrencyExchange from './js/currencyExchange-service';
+
+async function callForUSDRates(){
+  const response = await CurrencyExchange.getUSDExchangeRates();
+  return response;
+}
+
+callForUSDRates();
